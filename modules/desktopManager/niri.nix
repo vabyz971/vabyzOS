11384088954bd4a96bf0e347897c8560be9c5ea4
vabyz971 <vabyz971@gmail.com>
2025-12-28 20:33:28 -0500

    remove old file

--- modules/desktopManager/niri.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  programs.niri.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    niri
-    xwayland-satellite
-  ];
-}
--- modules/home/noctalia/default.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-{
-  imports = [
-    ./noctalia.nix
-  ];
-}
