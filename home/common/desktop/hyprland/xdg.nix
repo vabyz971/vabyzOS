3eb7e782de5883026aa15daa551d12dc04c8b804
vabyz971 <vabyz971@gmail.com>
2026-01-14 21:29:09 -0500

    correction bug xdg flatpak

---
+++ home/common/desktop/hyprland/xdg.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  xdg.portal = {
+    enable = true;
+    # xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+}
