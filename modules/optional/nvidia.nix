e2129fcd6ef14f53c585ce8631962e297d4c20b1
vabyz971 <vabyz971@gmail.com>
2026-09-10 18:29:41 -0400

    update Driver nvidia 615.71.09

--- modules/optional/nvidia.nix
+++ modules/optional/nvidia.nix
@@ -55,12 +55,12 @@
 
     # Version spécifique du driver nvidia
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "610.57.04";
-      sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
-      sha256_aarch64 = "sha256-QCefrMBCmpOwuOyXv1k5Gj0iB2CYlPgnG3JToUw/j54=";
-      openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
-      settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
-      persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
+      version = "615.71.09";
+      sha256_64bit = "sha256-zc7tIrvrYSSNGm3qvCWWZz46ZQFpjucayNL9wo87cP4=";
+      sha256_aarch64 = "";
+      openSha256 = "";
+      settingsSha256 = "sha256-LK1LU8mDkM/XVRKPBtuOZh9nIP/lGFLAJnmasEX8jhg=";
+      persistencedSha256 = "";
     };
   };
 }
